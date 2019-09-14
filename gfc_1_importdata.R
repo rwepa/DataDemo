bd6653e64dc952ade5bb19e2ad45223655d63406
MING-CHANG LEE <36437869+rwepa@users.noreply.github.com>
2019-09-14 11:45:45 +0800

    Create gfc_1_importdata.R

---
+++ gfc_1_importdata.R
@@ -0,0 +1,20 @@
+# title    : 匯入 gfc 資料集
+# date     : 2019.9.14
+# author   : Ming-Chang Lee
+# email    : alan9956@gmail.com
+# RWEPA    : http://rwepa.blogspot.tw/
+# Encoding : UTF-8
+
+# 方法1 有錯誤訊息
+# gfc <- read.table("gfc.csv", header=TRUE, sep=",")
+
+# 方法2 正確匯入資料
+
+# 在 Windows 作業系統中, 使用記事本開啟 gfc.csv, 另存新檔 ANSI 編碼格式, 檔名改為 gfc-ansi.csv
+
+gfc <- read.table("gfc-ansi.csv", header=TRUE, sep=",")
+gfc
+str(gfc) # 293筆資料,3個欄位
+summary(gfc)
+plot(gfc$amount, type="l") # 應如何進行資少分析?
+# end
