c4195c042c89c67b2ac8490815897678eb14c621
MING-CHANG LEE <36437869+rwepa@users.noreply.github.com>
2019-09-14 11:18:59 +0800

    Update gfc.R

--- gfc.R
+++ gfc.R
@@ -1,5 +1,6 @@
 # import data
 # 使用記事本開啟 gfc.csv, 另存新檔 ANSI 編碼格式, 檔名改為 gfc-ansi.csv
+gfc <- read.table("gfc.csv", header=TRUE, sep=",") # Error in Windows OS
 gfc <- read.table("gfc-ansi.csv", header=TRUE, sep=",")
 gfc
 str(gfc) # 293筆資料,3個欄位
