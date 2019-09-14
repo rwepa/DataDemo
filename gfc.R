79c73e3775c92d362298a8b939c5ae4723bb2d5c
MING-CHANG LEE <36437869+rwepa@users.noreply.github.com>
2019-09-14 11:15:11 +0800

    Update gfc.R

--- gfc.R
+++ gfc.R
@@ -1,7 +1,8 @@
 # import data
-gfc <- read.table("gfc.csv", header=TRUE, sep=",")
+# 使用記事本開啟 gfc.csv, 另存新檔 ANSI 編碼格式, 檔名改為 gfc-ansi.csv
+gfc <- read.table("gfc-ansi.csv", header=TRUE, sep=",")
 gfc
-str(gfc)
+str(gfc) # 293筆資料,3個欄位
 summary(gfc)
-plot(gfc$amount, type="l")
+plot(gfc$amount, type="l") # 應如何進行資少分析?
 # end
