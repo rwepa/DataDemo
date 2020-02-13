35ab3cf0d8a0dc5506180d2a969065296d858061
MING-CHANG LEE <36437869+rwepa@users.noreply.github.com>
2020-02-13 17:46:25 +0800

    Update 2019nCoV.R

--- 2019nCoV.R
+++ 2019nCoV.R
@@ -13,27 +13,39 @@ library(htmltools) # tag
 # 確診 Confirmed
 ncovConfirmedUrl <- "https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/time_series/time_series_2019-ncov-Confirmed.csv"
 
+# 匯入資料
 ncovConfirmed <- read.table(ncovConfirmedUrl, header=TRUE, sep=",")
 
+# 將 X 取代為空白
 names(ncovConfirmed) <- gsub("X", "", names(ncovConfirmed))
 
+# 使用strsplit, 依據 "." 進行字串分割
+# unlist 將分割的 list 轉換為向量
+# matrix 的 byrow=TRUE 可以將資料依玥順序排列
+# as.data.frame 轉換為 data.frame 方便後序操作
 mydf <- as.data.frame(matrix(unlist(strsplit(names(ncovConfirmed)[-c(1:4)], "\\.")), ncol=5, byrow=TRUE))
 
+# 儲存轉換後的欄位名稱向量
 myname <- c()
+
+# 將欄位名稱 "1.21.20.22.00" 轉換為 "2020-1-21 22:00", 即"年-月-日 時:分"格式
 for (i in 1:nrow(mydf)) {
   myname <- c(myname, paste0(paste0("20", mydf[i, 3]), "-", mydf[i, 1], "-", mydf[i, 2], " ", mydf[i, 4], ":", mydf[i, 5]))
 }
 
+# 刪除 mydf 物件
 rm(mydf)
 
 names(ncovConfirmed)[-c(1:4)] <- myname
 
 head(ncovConfirmed)
 
+# 使用 aggregate 計算各國家 2020-2-10 19:30 的合計, 須使用 ‵  ‵ 字元
 ncovConfirmedAgg <- aggregate(`2020-2-10 19:30` ~ Country.Region, 
   data=ncovConfirmed, 
   sum)
 
+# 使用 aggregate 計算各國家最近日期的合計
 ncovConfirmedAgg <- aggregate(get(names(ncovConfirmed)[ncol(ncovConfirmed)]) ~ Country.Region, 
   data=ncovConfirmed, 
   sum)
