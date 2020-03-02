e1e3941c8a6ef94e20fcb666333ec6f272148348
MING-CHANG LEE <36437869+rwepa@users.noreply.github.com>
2020-03-02 14:22:16 +0800

    Update 2019nCoV.R

--- 2019nCoV.R
+++ 2019nCoV.R
@@ -1,5 +1,6 @@
 # title: 2019新型冠狀病毒視覺化
 # date: 2020.2.13
+# updated: 2020.3.2
 # author: Ming-Chang Lee
 # email: alan9956@gmail.com
 # RWEPA: http://rwepa.blogspot.tw/
@@ -23,15 +24,17 @@ names(ncovConfirmed) <- gsub("X", "", names(ncovConfirmed))
 # unlist 將分割的 list 轉換為向量
 # matrix 的 byrow=TRUE 可以將資料依玥順序排列
 # as.data.frame 轉換為 data.frame 方便後序操作
-mydf <- as.data.frame(matrix(unlist(strsplit(names(ncovConfirmed)[-c(1:4)], "\\.")), ncol=5, byrow=TRUE))
+# 月 日 年
+mydf <- as.data.frame(matrix(unlist(strsplit(names(ncovConfirmed)[-c(1:4)], "\\.")), ncol=3, byrow=TRUE))
 
 # 儲存轉換後的欄位名稱向量
 myname <- c()
 
-# 將欄位名稱 "1.21.20.22.00" 轉換為 "2020-1-21 22:00", 即"年-月-日 時:分"格式
+# 將欄位名稱 "1.22.20" 轉換為 "2020-1-21", 即 "年-月-日" 格式
 # for (i in 1:...) { }, nrow(mydf) 表示列個數
+
 for (i in 1:nrow(mydf)) {
-  myname <- c(myname, paste0(paste0("20", mydf[i, 3]), "-", mydf[i, 1], "-", mydf[i, 2], " ", mydf[i, 4], ":", mydf[i, 5]))
+  myname <- c(myname, paste0(paste0("20", mydf[i, 3]), "-", mydf[i, 1], "-", mydf[i, 2]))
 }
 
 # 刪除 mydf 物件
@@ -42,13 +45,14 @@ names(ncovConfirmed)[-c(1:4)] <- myname
 head(ncovConfirmed)
 
 # 使用 aggregate 計算各國家 2020-2-10 19:30 的合計, 須使用 ‵  ‵ 字元
-ncovConfirmedAgg <- aggregate(`2020-2-10 19:30` ~ Country.Region, 
+ncovConfirmedAgg <- aggregate(`2020-3-1` ~ Country.Region, 
                               data=ncovConfirmed, 
                               sum)
 
 # 使用 aggregate 計算各國家最近日期的合計
 # names(ncovConfirmed)[ncol(ncovConfirmed)] 取出最後一個欄位名稱,其結果為字串
 # get(names(ncovConfirmed)[ncol(ncovConfirmed)]) 將欄位名稱轉換為物件名稱
+
 ncovConfirmedAgg <- aggregate(get(names(ncovConfirmed)[ncol(ncovConfirmed)]) ~ Country.Region, 
                               data=ncovConfirmed, 
                               sum)
@@ -66,11 +70,12 @@ ncovDeath <- read.table(ncovDeath, header=TRUE, sep=",")
 
 names(ncovDeath) <- gsub("X", "", names(ncovDeath))
 
-mydf <- as.data.frame(matrix(unlist(strsplit(names(ncovDeath)[-c(1:4)], "\\.")), ncol=5, byrow=TRUE))
+mydf <- as.data.frame(matrix(unlist(strsplit(names(ncovDeath)[-c(1:4)], "\\.")), ncol=3, byrow=TRUE))
 
 myname <- c()
+
 for (i in 1:nrow(mydf)) {
-  myname <- c(myname, paste0(paste0("20", mydf[i, 3]), "-", mydf[i, 1], "-", mydf[i, 2], " ", mydf[i, 4], ":", mydf[i, 5]))
+  myname <- c(myname, paste0(paste0("20", mydf[i, 3]), "-", mydf[i, 1], "-", mydf[i, 2]))
 }
 
 rm(mydf)
@@ -79,7 +84,7 @@ names(ncovDeath)[-c(1:4)] <- myname
 
 head(ncovDeath)
 
-ncovDeathAgg <- aggregate(`2020-2-10 19:30` ~ Country.Region, data=ncovDeath, sum)
+ncovDeathAgg <- aggregate(`2020-3-1` ~ Country.Region, data=ncovDeath, sum)
 
 ncovDeathAgg <- aggregate(get(names(ncovDeath)[ncol(ncovDeath)]) ~ Country.Region, 
                           data=ncovDeath, 
