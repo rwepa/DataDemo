c9f0f1741eb61a54573f6328afd953aff9f7e8ec
MING-CHANG LEE     (李明昌) <36437869+rwepa@users.noreply.github.com>
2020-07-25 23:40:04 +0800

    Update sp500.R

--- sp500.R
+++ sp500.R
@@ -1,30 +1,46 @@
-# sp500 data processing
+# title    : sp500.R
+# date     : 2020.7.25
+# author   : Ming-Chang Lee
+# email    : alan9956@gmail.com
+# RWEPA    : http://rwepa.blogspot.tw/
+# Encoding : UTF-8
+
+# sp500 資料處理與視覺化
 
 # source: https://github.com/rwepa/DataDemo/blob/master/sp500.RData
 
 library(zoo)
 
+library(tidyverse)
+
 load("sp500.RData")
 
-str(sp500) # data.frame, 16607*7
+str(sp500) # spec_tbl_df’, ‘tbl_df’, ‘tbl’ and 'data.frame
 
 head(sp500)
 
 tail(sp500)
 
-# data processing
+# 方法1: 使用 ggplot2 視覺化
+sp500 %>%
+  ggplot(aes(x = date, y = close)) +
+  geom_line(size = 1) +
+  labs(title = "sp500收盤價",
+       x = "Date", y = "Close")
 
-sp500 <- read.zoo(sp500, format = "%Y-%m-%d")
+# 方法2: 轉換為 zoo 物件並進行視覺化
 
-head(sp500)
+mydf <- read.zoo(sp500, format = "%Y-%m-%d")
 
-tail(sp500)
+head(mydf)
 
-str(sp500) # zoo
+tail(mydf)
 
-plot(sp500$close,
+str(mydf) # zoo
+
+plot(mydf$close,
      main = "SP500 (1950-2015)",
-     xlab="Year",
+     xlab="Date",
      ylab="Close")
 
 grid()
