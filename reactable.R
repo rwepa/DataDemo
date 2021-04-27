a201ba9c286eb58e27510ac5bd21c82435d1abb7
MING-CHANG LEE     (李明昌) <36437869+rwepa@users.noreply.github.com>
2021-04-27 21:46:38 +0800

    Create reactable.R

---
+++ reactable.R
@@ -0,0 +1,28 @@
+# date     : 2021.04.27
+# author   : Ming-Chang Lee
+# email    : alan9956@gmail.com
+# RWEPA    : http://rwepa.blogspot.tw/
+# Encoding : UTF-8
+
+# 範例1
+library(reactable)
+
+reactable(CO2)
+
+# 範例2
+library(shiny)
+
+library(reactable)
+
+ui <- fluidPage(
+  titlePanel("reactable package example"),
+  reactableOutput("table")
+)
+
+server <- function(input, output, session) {
+  output$table <- renderReactable({
+    reactable(CO2)
+  })
+}
+
+shinyApp(ui, server)
