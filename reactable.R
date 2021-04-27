99b56161f5760bbc48f8eab1a96092ba81d878bd
MING-CHANG LEE     (李明昌) <36437869+rwepa@users.noreply.github.com>
2021-04-27 22:55:43 +0800

    Update reactable.R

--- reactable.R
+++ reactable.R
@@ -1,15 +1,16 @@
+# title    : reactable package
 # date     : 2021.04.27
 # author   : Ming-Chang Lee
 # email    : alan9956@gmail.com
 # RWEPA    : http://rwepa.blogspot.tw/
 # Encoding : UTF-8
 
-# 範例1
+# 範例1 - 表格應用
 library(reactable)
 
 reactable(CO2)
 
-# 範例2
+# 範例2 - shiny 加上表格應用
 library(shiny)
 
 library(reactable)
@@ -26,4 +27,27 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui, server)
+
+# 範例3 - 表格加上折線圖 sparkline
+
+library(dplyr)
+library(sparkline)
+library(reactable)
+
+mydata <- iris %>%
+  group_by(Species) %>%
+  summarise(width = list(Petal.Width)) %>%
+  mutate(boxplot = NA, sparkline = NA)
+
+reactable(mydata, columns = list(
+  width = colDef(cell = function(values) {
+    sparkline(values, type = "bar", chartRangeMin = 0, chartRangeMax = max(iris$Petal.Width))
+  }),
+  boxplot = colDef(cell = function(value, index) {
+    sparkline(mydata$width[[index]], type = "box")
+  }),
+  sparkline = colDef(cell = function(value, index) {
+    sparkline(mydata$width[[index]])
+  })
+))
 # end
