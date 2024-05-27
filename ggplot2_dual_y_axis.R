f31f4ec83bb96273d63202a8772161674d80a79c
MING-CHANG LEE     (李明昌) <36437869+rwepa@users.noreply.github.com>
2024-05-27 13:49:40 +0800

    Update ggplot2_dual_y_axis.R

--- ggplot2_dual_y_axis.R
+++ ggplot2_dual_y_axis.R
@@ -1,37 +1,77 @@
 # file     : ggplot2_dual_y_axis.R
+# title    : Dual Y axis with ggplot2
 # author   : Ming-Chang Lee
 # date     : 2017.09.09
+# updated  : 2024.5.27
 # YouTube  : https://www.youtube.com/@alan9956
 # RWEPA    : http://rwepa.blogspot.tw/
 # GitHub   : https://github.com/rwepa
 # Email    : alan9956@gmail.com
 
+# 2024.5.27 - 更新ggplot2 - dual y axis標題等語法
+
+# 載入狩件
 library(XML)
 library(ggplot2)
+
+# 下載套件清單
 urls <- "http://cran.csie.ntu.edu.tw/web/packages/available_packages_by_date.html"
 mydf <- readHTMLTable(urls, stringsAsFactors = FALSE)
-str(mydf)
+str(mydf) # list
+
+# 取出list的第1個資料框物件
 mydf <- mydf[[1]]
+
+# 刪除欄位名稱中的前後空白字元
 names(mydf) <- trimws(names(mydf))
+
+# 將Date欄位轉換為日期字元
 mydf$Date <- as.Date(mydf$Date)
+
+# 計算每年為群組的套件個數
 mydf <- data.frame(table(format(mydf$Date, "%Y")), stringsAsFactors=FALSE)
-mydf$AccumulatedPAckages <- cumsum(mydf$Freq)
+
+# 新增每年為群組的累計套件個數
+mydf$AccumulatedPackages <- cumsum(mydf$Freq)
+
+# 更改欄位名稱
 names(mydf)[1:2] <- c("Year", "Packages")
+
+# 檢視資料集
 mydf
+
 ggplot(mydf, aes(x = Year)) +
+  # 建立"Packages per year"長條圖, 另可使用 geom_bar {ggplot2} 繪製.
   geom_col(aes(y = Packages*2), fill="grey") + 
+  
+  # 繪製累計套件數線圖
+  geom_line(aes(y = AccumulatedPackages, group = 1), linetype = 2, color = "blue") +
+  
+  # 繪製累計套件數點圖
+  geom_point(aes(y = AccumulatedPackages, group = 1), color = "blue") +
+  
+  # 標記累計套件數文字
+  geom_text(aes(y = AccumulatedPackages, label = AccumulatedPackages), 
+            vjust = -0.3, size = 3.5, color = "blue") +
+  
+  # 設定主要標題與左右置中
+  ggtitle("CRAN Packages Chart - Powered by RWEPA[https://rwepa.blogspot.com/]") + 
   theme(plot.title = element_text(hjust = 0.5)) + 
-  # xlab("年") + 
-  xlab(paste0("年 (2005-01 ~ ", format(Sys.Date(), "%Y-%m"), ")")) +
-  ylab("累計套件數") +
-  ggtitle("CRAN-套件統計圖 - by RWEPA") + 
-  scale_y_continuous(sec.axis = sec_axis(trans = ~. /2)) + 
-  theme(axis.title.y = element_text(colour = "blue")) + 
-  annotate("text", x=13, y=5500, label= "套件數(年)", angle = -90) + 
-  geom_line(aes(y = AccumulatedPAckages, group = 1), linetype = 2, color = "blue") +
+  
+  # 設定X軸標題, Y軸標題
+  xlab(paste0("Year(2008.01 ~ ", format(Sys.Date(), "%Y.%m"), ")")) +
+  ylab("Accumulated packages") +
+  
+  # 設定第2Y軸刻度為主Y軸刻度的一半
+  scale_y_continuous(sec.axis = sec_axis(name = "Packages per year", trans = ~./2)) +
+  
+  # 設定X軸刻度標題位置旋轉45度與Y軸標題,第2Y軸標題顏色
   theme(axis.text.x = element_text(face="bold", angle=45),
-        axis.text.y = element_text(face="bold", color="black", size=10)) +
-  geom_point(aes(y = AccumulatedPAckages, group = 1), color = "blue") +
-  geom_text(aes(y = AccumulatedPAckages, label = AccumulatedPAckages), 
-            vjust = -0.3, size = 4, color = "blue")
+        axis.title.y = element_text(colour = "blue"),
+        axis.title.y.right = element_text(color = "black"))
+# 警告訊息：
+# The `trans` argument of `sec_axis()` is deprecated as of ggplot2 3.5.0.
+# ℹ Please use the `transform` argument instead.
+# This warning is displayed once every 8 hours.
+# Call `lifecycle::last_lifecycle_warnings()` to see where this warning was generated. 
 # end
